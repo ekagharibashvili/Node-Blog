f420bd712aadd37bdd6a1a8bbc29cd82c15b88a4
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-09 19:18:14 +0400

    feat: add validators

--- models/user.js
+++ models/user.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: [true, "User must have a unique username"],
+    minlength: [3, "Username must include at least three characters"],
+    maxlength: [20, "Username must include maximum twenty characters"],
     required: [true, "User must have a username"],
   },
   password: {
     type: String,
+    unique: [true, "User must have a unique password"],
+    minlength: [6, "Password must include at least six characters"],
+    maxlength: [12, "Password must include maximum twelve characters"],
     required: [true, "User must have a password"],
   },
   email: {
     type: String,
+    unique: [true, "User must have a unique email address"],
     required: [true, "User must have a email address"],
+    validate: [validator.isEmail, "Please provide a valid email"],
   },
   createdAt: {
     type: String,
