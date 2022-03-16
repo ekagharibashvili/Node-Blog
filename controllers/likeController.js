bea75afc509cec4771e9f337e6d9e97e4d04cdcf
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-16 23:35:31 +0400

    feat: add like routes

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,14 +1,53 @@
 const Like = require("../models/like");
 
 exports.makeLike = async (req, res, next) => {
-    try {
-      const like = await Like.create(req.body).populate("user");
-  
-      res.status(200).json({
-        status: "OK",
-        data: like,
-      });
-    } catch (err) {
-      console.log(JSON.stringify(err, null, 2));
-    }
-  };
+  try {
+    const like = await Like.create(req.body).populate("user post");
+
+    res.status(200).json({
+      status: "OK",
+      data: like,
+    });
+  } catch (err) {
+    console.log(JSON.stringify(err, null, 2));
+  }
+};
+
+exports.getLikes = async (req, res, next) => {
+  try {
+    const likes = await Like.find({}).populate("user post");
+
+    res.status(200).json({
+      status: "OK",
+      data: likes,
+    });
+  } catch (err) {
+    console.log(JSON.stringify(err, null, 2));
+  }
+};
+
+exports.updateLike = async (req, res, next) => {
+  try {
+    const like = await Like.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).populate("user post");
+    res.status(200).json({
+      status: "OK",
+      data: like,
+    });
+  } catch (err) {
+    console.log(JSON.stringify(err, null, 2));
+  }
+};
+
+exports.deleteLike = async (req, res, next) => {
+  try {
+    await Like.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "OK",
+      data: null,
+    });
+  } catch (err) {
+    console.log(JSON.stringify(err, null, 2));
+  }
+};
--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -6,5 +6,8 @@ const router = express.Router();
 router.use(isAuth);
 
 router.route("/make").post(commentController.makeLike);
+router.route("/get").get(commentController.getLikes);
+router.route("/update/:id").put(commentController.updateLike);
+router.route("/delete/:id").delete(commentController.deleteLike);
 
 module.exports = router;
