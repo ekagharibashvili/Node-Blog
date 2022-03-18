e246d1b2ce5429e4b26a18d49842297f8ce2d26d
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-18 13:58:20 +0400

    delere useless codelines

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,7 +1,3 @@
-const User = require("../models/user");
-const { promisify } = require("util");
-const jwt = require("jsonwebtoken");
-
 module.exports = function isAdmin() {
   return (req, res, next) => {
     if (req.user[0].role !== "admin") {
