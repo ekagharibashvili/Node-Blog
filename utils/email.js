cea011181c7300af6e4a35230df5481832f88ddb
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-04-04 18:15:47 +0400

    delete pass

--- utils/email.js
+++ utils/email.js
@@ -11,7 +11,7 @@ exports.sendMail = async (req, res) => {
     secure: false,
     auth: {
       user: "gharibashvili.e@gtu.ge",
-      pass: "enigmakripto123",
+      pass: "",
     },
     tls: {
       rejectUnauthorized: false,
